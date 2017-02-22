65866e71f557117ce842c39d0714d70884d1ce3e
-LMH- <pc20071139@gmail.com>
2017-02-22 22:51:26 +0100

    auto-scale x, y, z extent

--- experiment/js/bubble.js
+++ experiment/js/bubble.js
@@ -1,7 +1,7 @@
 function createBubble(el) {
-    // axis definition
+    // Axis definition
     var keyField = 'Food',
-        xField = 'Carbohydrate (g)',
+        xField = 'Fat (g)',
         yField = 'Fat (g)',
         radiusField = 'CO2 footprint',
         colorField = 'Category';
@@ -20,55 +20,52 @@ function createBubble(el) {
         width = 960 - margin.right,
         height = 500 - margin.top - margin.bottom;
 
-    // Various scales. These domains make assumptions of data, naturally.
-    var xScale = d3.scaleLinear().domain([0, 100]).range([0, width]),
-        yScale = d3.scaleLinear().domain([0, 100]).range([height, 0]),
-        radiusScale = d3.scaleSqrt().domain([0, 20]).range([0, 40]),
-        colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-
-    // The x & y axes.
-    var xAxis = d3.axisBottom(xScale),//.scale(xScale).ticks(12, d3.format(",d")),
-        yAxis = d3.axisLeft(yScale)//.orient("left");
-
-    // Create the SVG container and set the origin.
-    var svg = el.append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    // Add the x-axis.
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    // Add the y-axis.
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis);
-
-    // Add an x-axis label.
-    svg.append("text")
-        .attr("class", "x label")
-        .attr("text-anchor", "end")
-        .attr("x", width)
-        .attr("y", height - 6)
-        .text("income per capita, inflation-adjusted (dollars)");
-
-    // Add a y-axis label.
-    svg.append("text")
-        .attr("class", "y label")
-        .attr("text-anchor", "end")
-        .attr("y", 6)
-        .attr("dy", ".75em")
-        .attr("transform", "rotate(-90)")
-        .text("life expectancy (years)");
-
-
     // Load the data.
     d3.csv("../data/food.csv", function(data) {
-        console.log(data);
+        // Various scales. These domains make assumptions of data, naturally.
+        var xScale = d3.scaleLinear().domain(d3.extent(data, x)).range([0, width]),
+            yScale = d3.scaleLinear().domain(d3.extent(data, y)).range([height, 0]),
+            radiusScale = d3.scaleSqrt().domain(d3.extent(data, radius)).range([0, 40]),
+            colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+        // The x & y axes.
+        var xAxis = d3.axisBottom(xScale),//.scale(xScale).ticks(12, d3.format(",d")),
+            yAxis = d3.axisLeft(yScale)//.orient("left");
+
+        // Create the SVG container and set the origin.
+        var svg = el.append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        // Add the x-axis.
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        // Add the y-axis.
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+        // Add an x-axis label.
+        svg.append("text")
+            .attr("class", "x label")
+            .attr("text-anchor", "end")
+            .attr("x", width)
+            .attr("y", height - 6)
+            .text(xField);
+
+        // Add a y-axis label.
+        svg.append("text")
+            .attr("class", "y label")
+            .attr("text-anchor", "end")
+            .attr("y", 6)
+            .attr("dy", ".75em")
+            .attr("transform", "rotate(-90)")
+            .text(yField);
 
         // A bisector since many nation's data is sparsely-defined.
         var bisect = d3.bisector(function(d) { return d[0]; });
