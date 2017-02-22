500f1c69c55062007a5de1d80dec118c19dd7b99
-LMH- <pc20071139@gmail.com>
2017-02-22 20:54:25 +0100

    fix d3 link

--- experiment/js/bubble.js
+++ experiment/js/bubble.js
@@ -12,14 +12,14 @@ var margin = {top: 19.5, right: 19.5, bottom: 19.5, left: 39.5},
     height = 500 - margin.top - margin.bottom;
 
 // Various scales. These domains make assumptions of data, naturally.
-var xScale = d3.scale.log().domain([300, 1e5]).range([0, width]),
-    yScale = d3.scale.linear().domain([10, 85]).range([height, 0]),
-    radiusScale = d3.scale.sqrt().domain([0, 5e8]).range([0, 40]),
-    colorScale = d3.scale.category10();
+var xScale = d3.scaleLog().domain([300, 1e5]).range([0, width]),
+    yScale = d3.scaleLinear().domain([10, 85]).range([height, 0]),
+    radiusScale = d3.scaleSqrt().domain([0, 5e8]).range([0, 40]),
+    colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
 // The x & y axes.
-var xAxis = d3.svg.axis().orient("bottom").scale(xScale).ticks(12, d3.format(",d")),
-    yAxis = d3.svg.axis().scale(yScale).orient("left");
+var xAxis = d3.axisBottom(xScale),//.scale(xScale).ticks(12, d3.format(",d")),
+    yAxis = d3.axisLeft(yScale)//.orient("left");
 
 // Create the SVG container and set the origin.
 var svg = d3.select("#chart").append("svg")
