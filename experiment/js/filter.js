e30a66edf59e3dd720f115c4bd0d61d2f4b9cf98
Martin <martin.funkquist@gmail.com>
2017-03-01 22:04:19 +0100

    Fixed function for moving the slider button.

--- experiment/js/filter.js
+++ experiment/js/filter.js
@@ -63,17 +63,21 @@ function createScaler() {
   sliderHandle.append("div")
     .attr("class", "slider-handle-icon");
 
-  slider.call(d3.drag()
-    .on("start", function() {
-      dispatch.sliderChange(x.invert(d3.mouse(sliderTray.node())[0]));
-      d3.event.sourceEvent.preventDefault();
-    })
-    .on("drag", function() {
-      dispatch.sliderChange(x.invert(d3.mouse(sliderTray.node())[0]));
-    }));
-
   dispatch.on("sliderChange.slider", function(value) {
     sliderHandle.style("left", x(value) + "px")
   });
+
+  slider.call(d3.drag()
+    .on("start", function() {
+      dispatch.call("sliderChange", this, x.invert(d3.mouse(sliderTray.node())[0]));
+
+      //dispatch.sliderChange(x.invert(d3.mouse(sliderTray.node())[0]));
+      d3.event.sourceEvent.preventDefault();
+    })
+    .on("drag", function() {
+      dispatch.call("sliderChange", this, x.invert(d3.mouse(sliderTray.node())[0]));
+      //dispatch.sliderChange(x.invert(d3.mouse(sliderTray.node())[0]));
+    }));
+
 
 }
