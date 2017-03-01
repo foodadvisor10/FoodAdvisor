84865e2e2048c2d14058eb0f2fcaa01eb24b4015
Martin <martin.funkquist@gmail.com>
2017-03-01 15:59:46 +0100

    Added a slider

--- experiment/js/filter.js
+++ experiment/js/filter.js
@@ -8,9 +8,11 @@ var filters = [
   "Proteins",
   "Calories"
 ];
+
+var scalerWidth = 100;
 //Set button listener
 function addFilterButtonListener() {
-  var button = $("#filter-button");
+  var button = $("#add-filter-button");
   button.on("click", createNewFilter);
 }
 
@@ -18,12 +20,12 @@ function addFilterButtonListener() {
 function createNewFilter(){
 
   var dropdown = d3.select("#filter-table")
-    .append("tr")
+    .append("tr").attr("id", "filterRow")
     .append("td")
     .append("select").attr("class", "filterList");
 
   //Append the options to the dropdown
-  var options = dropdown.selectAll("option")
+  dropdown.selectAll("option")
     .data(filters)
     .enter()
     .append("option")
@@ -33,4 +35,45 @@ function createNewFilter(){
       .text(function (d) {
         return d;
       });
+
+  dropdown.on("change", function () {
+    createScaler();
+  })
+}
+
+//Function for creating the scaler when a filter is selected
+function createScaler() {
+  //Add the scaler
+  var x = d3.scaleLinear()
+    .domain([0, 100])   //TODO: Change this to the max and min values of the filter selected
+    .range([0, scalerWidth]);
+
+  var dispatch = d3.dispatch("sliderChange");
+
+  var slider = d3.select("#filterRow").append("div")
+    .attr("class", "slider")
+    .style("width", scalerWidth + "px");
+
+  var sliderTray = slider.append("div")
+    .attr("class", "slider-tray");
+
+  var sliderHandle = slider.append("div")
+    .attr("class", "slider-handle");
+
+  sliderHandle.append("div")
+    .attr("class", "slider-handle-icon");
+
+  slider.call(d3.drag()
+    .on("start", function() {
+      dispatch.sliderChange(x.invert(d3.mouse(sliderTray.node())[0]));
+      d3.event.sourceEvent.preventDefault();
+    })
+    .on("drag", function() {
+      dispatch.sliderChange(x.invert(d3.mouse(sliderTray.node())[0]));
+    }));
+
+  dispatch.on("sliderChange.slider", function(value) {
+    sliderHandle.style("left", x(value) + "px")
+  });
+
 }
