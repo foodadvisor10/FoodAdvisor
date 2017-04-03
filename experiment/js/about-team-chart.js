9dde4c6c33178bfb3fdc294347d74e78d180ee66
jsvaliente <jsvaliente@gmail.com>
2017-04-03 23:24:54 +0200

    Clickeable slices
    Only works in the tab “Joaquin”

--- experiment/js/about-team-chart.js
+++ experiment/js/about-team-chart.js
@@ -207,9 +207,21 @@ function change(data) {
         .on("mouseout", function(d){
             div.style("display", "none");
         });
+		slice
+				.on("click", function(d){
+					toggle_visibility(d.data.label);
+				});
 
     slice.exit()
         .remove();
+
+				function toggle_visibility(id) {
+					var e = document.getElementById(id);
+					if(e.style.display == 'block')
+					e.style.display = 'none';
+					else
+					e.style.display = 'block';
+				}
 
     /* ------- TEXT LABELS -------*/
 
